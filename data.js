d49be40d17cc50751df3cab2c36ccab908b98e42
Sanaaaaaaaa04 <123sanask@gmail.com>
2024-12-12 14:48:16 +0530

    2nd

---
+++ data.js
@@ -0,0 +1,8 @@
+const data = [
+    {name: 'Khirad', email:'khirad@gmail.com'},
+    {name: 'Hania', email:'hania@gmail.com'},
+    {name: 'Shibra', email:'shibra@gmail.com'},
+    {name: 'Dure', email:'dure@gmail.com'}
+]
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
-http.createServer((req, resp) =>{
-    resp.write("<h1>Hello Guysssss Welcome Back to My Channel !!</h1> ");
+
+http.createServer((req, resp) => {
+    resp.write("<h1>Hello Guysssss, Welcome Back to My Channel !!</h1> ");
     resp.end();
 }).listen(4500);
 
